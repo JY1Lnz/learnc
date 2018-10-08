cc7d434d25d3bd801a1314b0e1a314ad51c96530
晋毓麟 <2522471070@qq.com>
2018-10-08 14:52:09 +0800

    2.0 dynamic array 链表实现

--- c_dynamic_array.c
+++ c_dynamic_array.c
@@ -1,43 +1,42 @@
 #include<stdio.h>
 //定义两个指针 给与内存 相互赋值完成
 //此程序 当输入为0时，判定下一个输入的数赋值到数组内
+typedef struct array{
+    int data;
+    struct array * next;
+}Dyarray;
+
+void Init(Dyarray * L)
+{
+    L -> next = NULL;
+}
+
+void Insert(Dyarray * L,int e)
+{
+    Dyarray * p = L,* s;
+    while(p->next != NULL)
+    {
+        p = p->next;
+    }
+    s = (Dyarray *)malloc(sizeof(Dyarray));
+    s -> data = e;
+    s -> next = NULL;
+    p -> next = s;
+}
 int main()
 {
-    int * data_now;
-    int * data_last;
-    int last = 0;
-    int now = 0;
+    Dyarray array;
+    Init(&array);
     int n;
-    int length = 0;
     while(scanf("%d",&n) != EOF)
     {
-        if (n == 0)
-        {
-            int a;
-            scanf("%d",&a);
-            ++length;
-            data_now = (int *)malloc(length*sizeof(int));
-            if(length>1)
-            {
-                for (int i = 0;i<length-1;i++)
-                    *(data_now+i) = *(data_last+i);
-                *(data_now+(length-1)) = a;
-                data_last = (int *)malloc(length*sizeof(int));
-                for (int i = 0;i<length;i++)
-                    *(data_last+i) = *(data_now+i);
-            }
-            else
-            {
-                data_now = (int *)malloc(length*sizeof(int));
-                data_last = (int *)malloc(length*sizeof(int));
-                *(data_now) = a;
-                *(data_last) = a;
-            }
-        }
+        Insert(&array,n);
     }
-    for (int i = 0;i<length;i++)
+    Dyarray * p = &array;
+    while(p->next != NULL)
     {
-        printf("%d  ",*(data_now+i));
+        printf("%d ",p->next->data);
+        p = p -> next;
     }
 
 	return 0;
